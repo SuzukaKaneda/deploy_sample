933c84c24648cf67827a3b18085cca24a22a788f
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-02-18 19:21:09 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_17_101508) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_18_095451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,5 +18,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_17_101508) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
   end
 end
