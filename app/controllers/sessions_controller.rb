c0a57dc1f382b4b3f3ea2115c8abf6750798d53f
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-02-19 19:34:18 +0900

    login2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-  user = User.find_by(email: params[:session][:email].downcase)
-  if user && user.authenticate(params[:session][:password])
-    log_in user
-    flash[:success] = "ログインしました"
-    
-    # AJAXの場合はリダイレクト先のURLをJSONで返す
-    respond_to do |format|
-      format.html { redirect_to root_url }
-      format.json { render json: { redirect_url: root_url } }
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = "ログインしました"
+      
+      respond_to do |format|
+        format.html { redirect_to root_url } # HTMLリクエストの場合
+        format.json { render json: { redirect_url: root_url } } # JSONリクエストの場合
+      end
+    else
+      render 'new'
     end
-  else
-    render 'new'
   end
-end
 
   def destroy
     log_out if logged_in?
