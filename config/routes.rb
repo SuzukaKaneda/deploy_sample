c52ecc75a8e15aea6b8b71cddd48394ebabe196c
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-02-20 17:31:52 +0900

    user_create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
+  get "users/new"
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
+
+  resources :users
 
   resources :posts
     root "posts#index"
