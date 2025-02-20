0543231161630aa580afd48f1f9bf1b45ce55faf
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-02-20 18:52:41 +0900

    user5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
       flash[:success] = "ログインしました"
       redirect_to root_url
     else
