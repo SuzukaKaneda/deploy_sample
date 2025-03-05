e7e72bd5425d4f9385e2e313917b7ee64bc4d58c
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-03-05 14:29:52 +0900

    tag_表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-    mount_uploader :post_image, PostImageUploader
+  mount_uploader :post_image, PostImageUploader
+  has_many :post_tags,dependent: :destroy
+  has_many :tags,through: :post_tags
 end
